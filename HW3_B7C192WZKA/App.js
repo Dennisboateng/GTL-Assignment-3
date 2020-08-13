f087da1e6d2ee593a3b3d859adc8c5e1f45da796
Dennis Boateng <68543847+Dennisboateng@users.noreply.github.com>
2020-08-13 17:41:46 +0000

    Add files via upload

---
+++ HW3_B7C192WZKA/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewAssign from './week-3/Den-ass.js';
+
+import './week-3/css/components.css';
+import './week-3/css/global.css';
+import './week-3/css/layout.css';
+import './week-3/css/sample-wallpaper.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     
+
+     <NewAssign />
+    </div>
+  );
+}
+
+export default App;
